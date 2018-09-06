7d925b42fd9ed89147337949b3d0161f9a307f6f
Félix Saparelli <felix@passcod.name>
2018-09-06 13:57:32 +1200

    Support CA creating/issuing

--- README.md
+++ README.md
@@ -8,12 +8,13 @@ Writing test.example.com.key
 Writing test.example.com.crt
 
 $ certainly --inspect test.example.com.crt
-Self-signed certificate created 2018-09-05 18:52:04 UTC
-Expires on 2028-09-05 18:52:04 UTC
+Self-signed certificate
+Created on:   Sep  6 01:30:45 2018 GMT
+Expires on:   Sep  3 01:30:45 2028 GMT
 Domains:
-  - test.example.com
-  - test2.example.com
-  - foo.local
+ - test.example.com
+ - test2.example.com
+ - foo.local
 ```
 
 
@@ -33,6 +34,12 @@ With Cargo: `cargo install certainly`
 
     yay -S certainly-bin
 
+#### Debian, Ubuntu (deb)
+
+Download the deb file from GitHub Releases.
+
+#### Others
+
 Accepting contributions for more!
 
 
@@ -40,8 +47,13 @@ Accepting contributions for more!
 
  - `--std` will output both key and certificate to STDOUT instead of writing files.
  - `--double-std` will output the key to STDERR and the certificate to STDOUT instead, so redirection can be used to write or pipe files where needed efficiently. Take care of checking the key is actually formatted properly and not an error message though!
+
  - `--inspect` outputs terse information about the passed certificate file and exits.
 
+ - `--make-ca` creates a key/certificate pair suitable for issuing instead. Use with `--ca`.
+ - `--ca NAME` signs a certificate with a CA pair instead of self-signing. Provide only the common filename, without the `.crt` and `.key` extensions.
+
+See [the man page](./certainly.1.ronn) for more.
 
 ## Etc
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy_pedantic))]
+#![cfg_attr(feature = "cargo-clippy", allow(similar_names))]
 
 extern crate clap;
 extern crate openssl;
@@ -11,12 +12,12 @@ use openssl::ec::{EcGroup, EcKey};
 use openssl::error::ErrorStack;
 use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
-use openssl::pkey::{PKey, Private};
+use openssl::pkey::{PKey, PKeyRef, Private};
 use openssl::x509::extension::{
     AuthorityKeyIdentifier as AuthKey, BasicConstraints, KeyUsage, SubjectAlternativeName,
     SubjectKeyIdentifier as SubjectKey,
 };
-use openssl::x509::{X509, X509Builder, X509NameBuilder, X509v3Context};
+use openssl::x509::{X509, X509Builder, X509NameBuilder, X509Ref};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::{io, path::PathBuf};
@@ -58,6 +59,18 @@ fn main() -> Result<(), Ernum> {
                 .help("Show information about a certificate"),
         )
         .arg(
+            Arg::with_name("make-ca")
+                .long("make-ca")
+                .value_name("NAME")
+                .help("Create a CA cert and key suitable for signing"),
+        )
+        .arg(
+            Arg::with_name("ca")
+                .long("ca")
+                .value_name("NAME")
+                .help("Issue a certificate using a CA cert and key"),
+        )
+        .arg(
             Arg::with_name("std")
                 .long("std")
                 .help("Output to stdout instead of writing files"),
@@ -78,39 +91,51 @@ fn main() -> Result<(), Ernum> {
         return inspect(args.value_of("inspect").unwrap().into());
     }
 
-    if !args.is_present("DOMAIN") {
+    if !(args.is_present("DOMAIN") || args.is_present("make-ca")) {
         eprintln!("{}", args.usage());
         std::process::exit(2);
     }
 
-    let (name, key, cert) = create(args.values_of("DOMAIN").unwrap().collect())?;
-    // unwrap is safe because it will have been caught by is_present()
+    let (name, key, cert) = if args.is_present("make-ca") {
+        let name: &str = args.value_of("make-ca").unwrap();
+        let (key, cert) = makeca(name)?;
+        (name.into(), key, cert)
+    } else {
+        let doms = args.values_of("DOMAIN").unwrap().collect();
+
+        if args.is_present("ca") {
+            let caname = args.value_of("ca").unwrap();
+            let cakey = load_key(format!("{}.key", caname).into())?;
+            let cacrt = load_cert(format!("{}.crt", caname).into())?;
+            create(doms, Some((cakey.as_ref(), cacrt.as_ref())))?
+        } else {
+            create(doms, None)?
+        }
+    };
 
     if args.is_present("double-std") {
-        io::stderr().write(&key)?;
-        io::stdout().write(&cert)?;
+        io::stderr().write_all(&key)?;
+        io::stdout().write_all(&cert)?;
     } else if args.is_present("std") {
-        io::stdout().write(&key)?;
-        io::stdout().write(&cert)?;
+        io::stdout().write_all(&key)?;
+        io::stdout().write_all(&cert)?;
     } else {
         let keyname = format!("{}.key", name);
         let crtname = format!("{}.crt", name);
 
         eprintln!("Writing {}", keyname);
         let mut keyfile = File::create(keyname)?;
-        keyfile.write(&key)?;
+        keyfile.write_all(&key)?;
 
         eprintln!("Writing {}", crtname);
         let mut crtfile = File::create(crtname)?;
-        crtfile.write(&cert)?;
+        crtfile.write_all(&cert)?;
     }
 
     Ok(())
 }
 
-fn create(domains: Vec<&str>) -> Result<(String, Vec<u8>, Vec<u8>), Ernum> {
-    let name = (*domains.get(0).unwrap()).into();
-
+fn base_cert(name: &str, ca: Option<(&PKeyRef<Private>, &X509Ref)>) -> Result<X509Builder, Ernum> {
     let mut subject = X509NameBuilder::new()?;
     subject.append_entry_by_text("C", "ZZ")?;
     subject.append_entry_by_text("ST", "AA")?;
@@ -122,14 +147,67 @@ fn create(domains: Vec<&str>) -> Result<(String, Vec<u8>, Vec<u8>), Ernum> {
     cert.set_version(2)?;
     cert.set_not_before(Asn1Time::days_from_now(0)?.as_ref())?;
     cert.set_not_after(Asn1Time::days_from_now(3650)?.as_ref())?;
-    cert.set_issuer_name(&subject)?;
     cert.set_subject_name(&subject)?;
+
+    let cacert = ca.map(|(_, c)| c);
+    cert.set_issuer_name(if let Some(cert) = cacert {
+        cert.subject_name()
+    } else {
+        &subject
+    })?;
 
     let mut serial = BigNum::new()?;
     serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
     cert.set_serial_number(serial.to_asn1_integer()?.as_ref())?;
 
     cert.append_extension(BasicConstraints::new().build()?)?;
+
+    let subjkey = SubjectKey::new().build(&cert.x509v3_context(cacert, None))?;
+    let authkey = AuthKey::new()
+        .keyid(false)
+        .issuer(false)
+        .build(&cert.x509v3_context(cacert, None))?;
+
+    cert.append_extension(subjkey)?;
+    cert.append_extension(authkey)?;
+
+    Ok(cert)
+}
+
+fn base_key() -> Result<PKey<Private>, Ernum> {
+    let curve = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1)?;
+    let eckey: EcKey<Private> = EcKey::generate(curve.as_ref())?;
+    Ok(PKey::from_ec_key(eckey)?)
+}
+
+fn makeca(name: &str) -> Result<(Vec<u8>, Vec<u8>), Ernum> {
+    let mut cert = base_cert(name, None)?;
+
+    cert.append_extension(
+        KeyUsage::new()
+            .critical()
+            .key_cert_sign()
+            .crl_sign()
+            .build()?,
+    )?;
+
+    let pkey = base_key()?;
+    cert.set_pubkey(pkey.as_ref())?;
+    cert.sign(pkey.as_ref(), MessageDigest::sha512())?;
+
+    let cert = cert.build();
+    let certpem = cert.to_pem()?;
+    let keypem = pkey.private_key_to_pem_pkcs8()?;
+    Ok((keypem, certpem))
+}
+
+fn create(
+    domains: Vec<&str>,
+    ca: Option<(&PKeyRef<Private>, &X509Ref)>,
+) -> Result<(String, Vec<u8>, Vec<u8>), Ernum> {
+    let name = domains[0];
+    let mut cert = base_cert(name, ca)?;
+
     cert.append_extension(
         KeyUsage::new()
             .critical()
@@ -139,31 +217,23 @@ fn create(domains: Vec<&str>) -> Result<(String, Vec<u8>, Vec<u8>), Ernum> {
             .build()?,
     )?;
 
-    fn ctx(cert: &X509Builder) -> X509v3Context {
-        cert.x509v3_context(None, None)
-    }
-
-    let subjkey = SubjectKey::new().build(&ctx(&cert))?;
-    let authkey = AuthKey::new()
-        .keyid(false)
-        .issuer(false)
-        .build(&ctx(&cert))?;
-
     let mut san = SubjectAlternativeName::new();
     for dom in domains {
-        san.dns(dom.into());
+        san.dns(dom);
     }
-    let san = san.build(&ctx(&cert))?;
-
-    cert.append_extension(subjkey)?;
-    cert.append_extension(authkey)?;
+    let san = san.build(&cert.x509v3_context(ca.map(|(_, c)| c), None))?;
     cert.append_extension(san)?;
 
-    let curve = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1)?;
-    let eckey: EcKey<Private> = EcKey::generate(curve.as_ref())?;
-    let pkey = PKey::from_ec_key(eckey)?;
+    let pkey = base_key()?;
     cert.set_pubkey(pkey.as_ref())?;
-    cert.sign(pkey.as_ref(), MessageDigest::sha512())?;
+    cert.sign(
+        if let Some((cakey, _)) = ca {
+            cakey
+        } else {
+            pkey.as_ref()
+        },
+        MessageDigest::sha512(),
+    )?;
 
     let cert = cert.build();
     let certpem = cert.to_pem()?;
@@ -171,51 +241,70 @@ fn create(domains: Vec<&str>) -> Result<(String, Vec<u8>, Vec<u8>), Ernum> {
     Ok((name.into(), keypem, certpem))
 }
 
-fn inspect(filepath: PathBuf) -> Result<(), Ernum> {
+fn load_cert(filepath: PathBuf) -> Result<X509, Ernum> {
     let mut file = File::open(filepath)?;
     let mut buf = vec![];
     file.read_to_end(&mut buf)?;
-    let cert = X509::from_pem(&buf)?;
+    Ok(X509::from_pem(&buf)?)
+}
+
+fn load_key(filepath: PathBuf) -> Result<PKey<Private>, Ernum> {
+    let mut file = File::open(filepath)?;
+    let mut buf = vec![];
+    file.read_to_end(&mut buf)?;
+    Ok(PKey::private_key_from_pem(&buf)?)
+}
+
+fn inspect(filepath: PathBuf) -> Result<(), Ernum> {
+    let cert = load_cert(filepath)?;
 
     let mut cname = None;
     let mut subjname: Vec<String> = vec![];
     for subj in cert.subject_name().entries() {
         let name = subj.object().nid().long_name()?;
         let data = subj.data().as_utf8()?;
-        subjname.push(format!("{}", name));
-        subjname.push(format!("{}", data));
+        subjname.push(name.to_string());
+        subjname.push(data.to_string());
         if name == "commonName" {
             cname = Some(data);
         }
     }
 
+    let mut iname = None;
     let mut issuname: Vec<String> = vec![];
     for issu in cert.issuer_name().entries() {
-        issuname.push(format!("{}", issu.object().nid().long_name()?));
-        issuname.push(format!("{}", issu.data().as_utf8()?));
+        let name = issu.object().nid().long_name()?;
+        let data = issu.data().as_utf8()?;
+        issuname.push(name.to_string());
+        issuname.push(data.to_string());
+        if name == "commonName" {
+            iname = Some(data);
+        }
     }
 
     if subjname == issuname {
         println!("Self-signed certificate");
+    } else if let Some(name) = iname {
+        println!("Certificate signed by {}", name);
     } else {
-        println!("Certificate (not self-signed!)");
+        println!("Certificate (signed)");
     }
 
     println!("Created on:   {}", cert.not_before());
     println!("Expires on:   {}", cert.not_after());
 
     match cert.subject_alt_names() {
-        None => match cname {
-            Some(name) => println!("Domains:\n - {}", name),
-            None => println!("No domains???"),
+        None => if let Some(name) = cname {
+            println!("Domains:\n - {}", name);
+        } else {
+            println!("No domains???");
         },
         Some(alts) => {
             println!("Domains:");
             for alt in alts {
-                match alt.dnsname() {
-                    Some(dns) => println!(" - {}", dns),
-                    None => {}
-                };
+                if let Some(dns) = alt.dnsname() {
+                    println!(" - {}", dns);
+                }
             }
         }
     };

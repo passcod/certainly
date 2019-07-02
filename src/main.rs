0284365871ca3960056a18ba3aff13b2d33b525f
Félix Saparelli <felix@passcod.name>
2019-07-02 17:53:43 +1200

    Pull hostname and create OU

--- src/main.rs
+++ src/main.rs
@@ -194,11 +194,16 @@ fn main() -> Result<(), Ernum> {
     Ok(())
 }
 
+lazy_static::lazy_static! {
+    static ref HOSTNAME: std::ffi::OsString = gethostname::gethostname();
+}
+
 fn distinguished(org: &str, name: &str) -> Result<X509Name, Ernum> {
     let mut dn = X509NameBuilder::new()?;
     dn.append_entry_by_text("C", "ZZ")?;
     dn.append_entry_by_text("ST", "AA")?;
     dn.append_entry_by_text("O", &format!("Certainly {}", org))?;
+    dn.append_entry_by_text("OU", &format!("{} from {}", name, HOSTNAME.to_string_lossy()))?;
     dn.append_entry_by_text("CN", name)?;
     Ok(dn.build())
 }
